cd24152008b5e8746ae4602a44006c4d28bd10d9
Fu3uK <dencw123@mail.ru>
2022-09-14 15:04:44 +0300

    Lab_1-2_Khotemskoy

---
+++ 06.js
@@ -0,0 +1,34 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+let stroke = 'строка'
+let num = 10
+let logic = false
+let nullA = null
+let undef = undefined
+let objectA = {
+
+}
+let mas = new Array()
+
+console.log(typeof(stroke))
+console.log(typeof(num))
+console.log(typeof(logic))
+console.log(typeof(nullA))
+console.log(typeof(undef))
+console.log(typeof(objectA))
+console.log(typeof(mas))
+
+
---
+++ 10.js
@@ -0,0 +1,13 @@
+/** ЗАДАЧА 10 - Let
+ *
+ * Измените код так, чтобы ошибка исчезла
+ */
+myFavoriteAnimal = 'Monkey'
+
+console.log(myFavoriteAnimal)
+    // 'Monkey'
+
+myFavoriteAnimal = 'Cat'
+
+console.log(myFavoriteAnimal)
+    // 'Cat'
